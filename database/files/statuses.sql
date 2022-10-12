da7cac2820379413078ed752ec354784163951a5
andreylasitsa <andrey.lasitsa@innowise-group.com>
2022-10-12 18:04:31 +0300

    migrations, seeders, models

---
+++ database/files/statuses.sql
@@ -0,0 +1,4 @@
+INSERT INTO statuses (id, name, created_at, updated_at)
+VALUES (1, 'PLANNED', NOW(), NOW()),
+       (2, 'IN PROGRESS', NOW(), NOW()),
+       (3, 'COMPLETED', NOW(), NOW());
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Exception;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use PDOException;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,14 +14,22 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      *
      * @return void
+     *
+     * @throws Exception
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $sql_dumps = [
+            './database/files/users.sql',
+            './database/files/statuses.sql',
+        ];
+        foreach ($sql_dumps as $file) {
+            $sql_dump = File::get($file);
+            try {
+                DB::connection()->getPdo()->exec($sql_dump);
+            } catch (PDOException $exception) {
+                throw new Exception('Error in ' . $file . ' ' . $exception->getMessage());
+            }
+        }
     }
 }
